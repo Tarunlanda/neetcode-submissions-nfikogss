a872bf935eb36eb346360e7656a7d081d4f44009
Landa Tarun <96141917+Tarunlanda@users.noreply.github.com>
2026-09-20 10:52:03 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        if(n!=m)
+        {
+            return false;
+        }
+        map<char,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+        }
+        for(int i=0;i<m;i++)
+        {
+            if(!mp[t[i]])
+            {
+                return false;
+            }
+            mp[t[i]]--;
+        }
+        return true;
+    }
+};
