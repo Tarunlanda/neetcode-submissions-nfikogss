2ef349b51135cc67eedd989075ecefed5aa0e7e0
Landa Tarun <96141917+Tarunlanda@users.noreply.github.com>
2026-09-20 10:46:15 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int n= nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(mp[nums[i]])
+            {
+                return true;
+            }
+            mp[nums[i]]++;
+        }
+        return false;
+    }
+};
